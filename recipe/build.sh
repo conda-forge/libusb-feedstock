43f0e189c02393fdc1efe59c34dffe8a1cb672cb
Ryan Volz <ryan.volz@gmail.com>
2021-04-30 13:40:44 -0400

    Disable static library build through configure and remove cxx dep.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,6 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
-./configure --prefix=${PREFIX}
+./configure --prefix=${PREFIX} --disable-static
 make
 make install
-
-# don't include static library in the package
-# CFEP-18 (https://github.com/conda-forge/cfep/blob/master/cfep-18.md)
-rm $PREFIX/lib/libusb*.a
